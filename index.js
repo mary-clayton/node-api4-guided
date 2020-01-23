49d066156028d61ee2c7f0cc1db3886cf6c83b99
Mary <mary.clayton1@hotmail.com>
2020-01-22 20:19:11 -0700

    guided

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
-
-const port = 4000;
+require('dotenv').config()
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
